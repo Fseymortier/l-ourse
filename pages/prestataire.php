38d67d6ebdaebf57336c528747c3da5b470e9387
Fseymortier <97294811+Fseymortier@users.noreply.github.com>
2024-04-30 20:23:36 +0200

    update
    ajout du contenu et stylisation des pages particulier et prestataire

--- pages/prestataire.php
+++ pages/prestataire.php
@@ -4,7 +4,22 @@ $h1 = 'Procédure d\'adhésion pour les prestataires';
 $txtHeader = 'Vous êtes prestataire et souhaitez accepter les paiments en OURSES ?';
 require_once('../composants/header.php');
 ?>
-<section>
-
+<section class="container_adherer">
+    <div class="container_item_adherer container_item_violet">
+        <h2 class="titles_adherer title_violet">Comment cela fonctionne-t-il ?</h2>
+        <div class="item_adherer">
+            <p>Pour pouvoir vous faire payer en OURSE dans votre commerce ou autre activité, vous devez être prestataire.
+                Vous remplissez une demande d'agrément en acceptant la charte. Faisable en ligne, en suivant ce lien. </p>
+            <p>La cotisation annuelle est de 20 euros, plus 10 euros par équivalent temps plein dans la structure. </p>
+            <p>Les OURSE obtenues peuvent ensuite être utilisées comme dépense personnelle du professionnel, paiement d'un part du revenu des salariés s'ils sont d'accord ou en règlement des factures de ses fournisseurs à hauteur maximum de 1 000 euros si ceux-ci sont aussi prestataires de l'OURSE.</p>
+        </div>
+    </div>
+    <div class="container_item_adherer container_item_jaune">
+        <h2 class="titles_adherer title_jaune">Communiquer entre prestataires</h2>
+        <div class="item_adherer">
+            <p>Je souhaite discuter entre prestataires pour améliorer la circulation de notre monnaie sur le territoire, ou pour tout autre chose...</p>
+            <p>Vous pouvez rejoindre le <a href="https://t.me/joinchat/M_MSehrHdXA-awj716j0Bw" class='colored_txt'>groupe Prestataires de l'OURSE</a> dans la messagerie Telegram et les salons d'échange écrit et vocal dans notre serveur <a href="https://discord.com/invite/9vWGGTs"  class='colored_txt'>Discord.</a></p>
+        </div>
+    </div>
 </section>
 <?php require_once('../composants/footer.php') ?>
